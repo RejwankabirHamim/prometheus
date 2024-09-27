bc025c7fcdb4c31225d42501d619000ecd9130ca
Hamim <hamim@appscode.com>
2024-09-27 18:02:33 +0600

    bash script added

---
+++ ping_test.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+
+# Loop to run the curl command 10 times to test the counter is increasing
+for i in {1..10}
+do
+  # Print the command to the terminal
+  echo "$i. Executing command: curl localhost:8181/ping"
+
+  # Execute the curl command
+  curl localhost:8181/ping
+
+done
